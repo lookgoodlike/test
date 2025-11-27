2e9ce87adf691454b8fa80fe1f6a392857d4a112
lookgoodlike <bostongira@gmail.com>
2025-11-27 21:55:19 +0100

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,12 @@
-console.log("Hello World from GitHub Pages!");
+console.clear();
+console.log("document.cookie:");
+console.log(document.cookie || "(нет доступных JS cookies)");
+
+console.log("\nРазбивка по ключам:");
+(document.cookie || "")
+  .split("; ")
+  .filter(Boolean)
+  .forEach(c => {
+    const [name, value] = c.split("=");
+    console.log(name, "=", value);
+  });
